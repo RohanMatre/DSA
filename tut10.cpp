80c99e70ad86c74cfb950ed95e96ec2a46733b46
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-07-02 20:18:29 +0530

    Add files via upload

---
+++ tut10.cpp
@@ -0,0 +1,96 @@
+//----------------------Episode-10:-For, While and do-while loops in C++-------------------
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  /* Loops in C++;
+  There are Three types of Loops in C++:-
+  1. For Loop
+  2. While Loop
+  3. Do-while Loop
+
+  1. For-Loop:-
+  Syntax of for-loop:-
+  for(initialiation;condition;updation)
+  {
+        Loop Body(C++ code)
+  }
+
+  Example - 1 Write a Number from 1 to 40 using for Loop.
+   int i;
+   for(i=1;i<=40;i++)
+  {
+      cout<<i<<endl;
+  }
+      return 0;
+  }
+  Example - 2 Write a Number of Infinite using for loop
+   for(i=1;34<=40;i++)
+  {
+       cout<<i<<endl;
+   }
+      return 0;
+  }
+
+  2. While-Loop-
+  Syntax of While-Loop:-
+  intialization
+  while(condition){
+      Loop Body
+      updatation
+  }
+
+  Example - 1 Write a Number from 1 to 40 using while Loop.
+   int i=1;
+   while(i<=40)
+   {
+       cout<<i<<endl;
+       i++;
+   }
+   return 0;
+   }
+
+  Example - 2 Write a Number of Infinite using while loop
+   int i=1;
+   while(true)
+   {
+     cout<<i<<endl;
+     i++;
+   }
+   return 0;
+   }*/
+
+  /* 3. do-while Loop:-
+  Syntax of do-while Loop:-
+  intialization
+  do{
+  loop body
+  }
+  while (condition);*/
+
+
+  // Example - 1 Write a Number from 1 to 40 using while Loop.
+  /*int i=1;
+  do{
+      cout<<i<<endl;
+      i++;
+  }while(i<=40);
+      return 0;
+  }*/
+
+//H.w. :- Write a Multipilcation Table of 6:-
+ int i,n=6;
+ for(i=1;i<=10;i++)
+ {
+    cout<<n*i<<endl;
+ }
+ return 0;
+}
+
+
+
+
+
