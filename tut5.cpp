8329d2082ab15407de73e5ffa8d30321953e0aa6
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-07-02 20:17:06 +0530

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,27 @@
+//------------->Episode-#05 C++ Basic Input/Output & More<----------------------
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int num1,num2;
+    cout<<"Enter the value of num1 :\n";  // This '<<' is called as Insertion operator 
+    cin>>num1;  //This '>>'  is called as Extraction operator
+    
+    cout<<"Enter the value of num2 :\n";  // This '<<' is called as Insertion operator 
+    cin>>num2;  //This '>>'  is called as Extraction operator
+    
+    cout<<"The Sum is : "<<num1+num2<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
