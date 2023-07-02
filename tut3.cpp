96dfed5e108075d1cb4deb744b8a23d6df147f60
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-07-02 20:16:38 +0530

    Add files via upload

---
+++ tut3.cpp
@@ -0,0 +1,12 @@
+//Episode-#02 & 03 Basic Structure & variables & comments:-  
+#include<iostream>//include-Function  iostream-Input Output stream Function(Header file)
+using namespace std;//for Standard Library of Specifed header file
+//---------------Theory of Comments-----------------------
+//------->This is Single Line comment
+/*  */ //-------> This is Multi - line Comments-----------------------
+//Shortcut Key to Select multi-line at the same time--->Ctrl+/ 
+int main(){
+    int sum = 6;
+    cout<<"Hello World"<<sum;
+    return 0;
+}
