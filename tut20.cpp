ce797f75b51c64b5245f87bfed6d8da6486a7aec
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-07-03 21:49:42 +0530

    Add files via upload

---
+++ tut20.cpp
@@ -0,0 +1,24 @@
+//--------------Object Oriented Programming in C++:- (V.V.V. IMP)---------
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Defintion's:- BASICS CONCEPT OF OOPS:- 
+    /* 1.) OOP's :- Set's of Function's
+       2.) POP(Procedure Oriented Programming) :- In Flow of Set's of Function's
+       3.) Classes :- Basic Template for Creating Objects.(Information Given to Create Object's)
+       4.) Data Abstraction(Separation of Concern) & Encapsulation :- Wrapping Data and Function's into Single Unit.
+       5.) Inheritance :- Properties of One Class can be Inherited into Other's.(Analog:- Properties from Parent's)
+       6.) Polymorphism :- Ability to take More than One Forms.
+       7.) Dynamic Binding(Late Binding) :- Code Which will Execute is not Known Until Program Runs. 
+       8.) Message Passing :- Object Message Call Format.
+    */
+    return 0;
+}
+
+
+
+
+
+
