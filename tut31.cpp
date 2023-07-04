fef283bb818fe41f51541ff657c8f45b8f8a7c38
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-07-04 21:26:44 +0530

    Add files via upload

---
+++ tut31.cpp
@@ -0,0 +1,41 @@
+//---------------------- Episode:- 31 : Constructor Overloading----------------------------
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+    int a, b;
+
+public:
+    Complex(){     // Default Constructor & Below 2 Constructor 
+        a=0;
+        b=0;
+    }
+    Complex(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+
+    Complex(int x)
+    {
+        a = x;
+        b = 0;
+    }
+    void printNumber()
+    {
+        cout << "Your Number is : " << a << " + " << b << " i " << endl;
+    }
+};
+int main()
+{
+    Complex c1(4, 6);
+    c1.printNumber();
+
+    Complex c2(5);
+    c2.printNumber();
+
+    Complex c3;
+    c3.printNumber();
+    return 0;
+}
