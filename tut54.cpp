53516226ed3d0ca019bb395c530a69a620c8cadf
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-07-06 22:44:53 +0530

    Add files via upload

---
+++ tut54.cpp
@@ -0,0 +1,24 @@
+//---------------------- Episode:- 54 : Polymorphism in C++ ----------------------------
+
+#include<iostream>
+using namespace std;
+
+/*
+                ------Polymorphism-------------
+               |                              |
+    Compile Time Polymorphism          Run Time Polymorphism
+               |                              |
+MEANS-OBJECT HAI WO function                  |
+CALL SE BOUND HOJATA HAI(EARLY BINDING) (LATE BINDING)
+               |                              |
+It is Acheived using:                  It is Acheived using:       
+(1) Function Overloading               (1) Virtual Functions
+(2) Operator Overloading
+*/
+int main(){
+    // Polymorphism :- Poly-many & Morphism-forms
+    // One Name and Multple forms(Role)
+    // Defination - e.g. (1) Functions Overloading (2) Operator overloading (3) Virtual functions
+    return 0;
+}
+
