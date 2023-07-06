2c7f35550cb11a0a9a3c17fbdba8a260c5f33798
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-07-06 22:42:07 +0530

    Add files via upload

---
+++ tut49.cpp
@@ -0,0 +1,46 @@
+// ---------Episode :- 49 : Initialization List in Constructors in Cpp --------
+#include <iostream>
+using namespace std;
+
+/*
+Syntax for initialization list in Constructor:-
+constructor (argument-list) : initialization-section
+{
+    assignment + other code;
+}
+
+class Test{
+    int a;
+    int b;
+    public:
+      Text(int i,int j) : a(i),b(j){constructor-body}
+};
+
+*/
+class Test
+{
+    int a;
+    int b;
+
+public:
+    //Test(int i, int j) : a(i), b(j)
+    //Test(int i, int j) : a(i), b(i+j)
+    //Test(int i, int j) : a(i), b(2 * j)
+    //Test(int i, int j) : a(i), b(a + j)
+    //Test(int i,int j) : b(j),a(i+b)  Shocked!! Garbage Value Due to Reason Initial variable(int a) will Run First Always.
+    // Red Flag,this will create a problem because a will be initialized first.
+    Test(int i,int j) : 
+    {  
+        a=i;
+        b=j;
+        cout << "Constructor Exceuted!!!" << endl; 
+        cout << "Value of a is "<<a<<endl; 
+        cout << "Value of b is "<<b<<endl; 
+    }
+};
+int main()
+{
+    Test t(4,6);
+
+    return 0;
+}
