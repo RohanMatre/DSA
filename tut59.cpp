50da113150f120738528ccb1deada4cd3982306b
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-07-09 18:07:01 +0530

    Add files via upload

---
+++ tut59.cpp
@@ -0,0 +1,20 @@
+// --------------- Episode :- 59 : File I/O in C++ ---------------------
+// File - Pattern/Sequence of pattern which is Store in Disk.  (# - ASCII(American Standard for Information Interchange) Value)
+// Ascii Value - https://www.w3schools.com/charsets/ref_html_ascii.asp
+// Ascii Value in Binary - https://www.roubaixinteractive.com/PlayGround/Binary_Conversion/The_Characters.asp
+
+ Files(write,read)    -------- ---C++ Program <----< Console Input(Keyboard)
+                      |        | --------
+   cout>>-Can print on screen  |        |------> cin>> - can be used to take input.
+        using cout             |
+                    Console output Screen/ Monitor
+                                
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
+
+
