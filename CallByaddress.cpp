5d090d790d916d8544f44f57590c46d63d623948
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-07-13 20:45:22 +0530

    Add files via upload

---
+++ CallByaddress.cpp
@@ -0,0 +1,22 @@
+// Call By Address 
+#include<iostream>
+using namespace std;
+
+int sum(int *,int *);
+
+int sum(int *x,int *y){
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+    return (*x+*y);
+}
+
+
+int main(){
+    int a=5,b=6;
+    int s=sum(&a,&b);
+    cout<<s<<endl;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    return 0;
+}
