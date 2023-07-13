46529e0f40d44fdcfaea284f779a668564cfbc02
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-07-13 20:45:51 +0530

    Add files via upload

---
+++ CallByvalue.cpp
@@ -0,0 +1,20 @@
+// Call By Value 
+#include<iostream>
+using namespace std;
+int sum(int ,int );
+
+int sum(int x,int y){
+    int temp=x;
+    x=y;
+    y=temp;
+    return (x+y);
+}
+
+
+int main(){
+    int a=5,b=6;
+    int s=sum(a,b);
+    cout<<s<<endl;
+    cout<<a<<endl;
+    cout<<b<<endl;
+}
