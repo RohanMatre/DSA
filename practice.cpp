4400ff02d4e144e37f628aaed8c42649b25e11f0
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-08-05 22:00:19 +0530

    Add files via upload

--- practice.cpp
+++ practice.cpp
@@ -1,36 +1,80 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-bool isPalindrome(int x)
+bool is_char(char c)
 {
-    int dup = x;
-    int revNum = 0;
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
 
-    if (x < 0)
-    {
-        return false;
-    }
-
-    while (x != 0)
-    {
-        int ld = x % 10;
-
-        if (revNum > INT_MAX || revNum < INT_MIN)
-        {
-            return 0;
-        }
-
-        revNum = ((revNum * 10) + ld);
-        x = x / 10;
-    }
-    if (revNum == dup) return true;
+bool is_num(char c)
+{
+    return (c >= '0' && c <= '9');
 }
 
 int main()
 {
-    int x;
-    cin >> x;
+    string n;
+    cout << "Enter the str: " << endl;
+    cin >> n;
 
-    isPalindrome(x);
+    int t;
+    cout << "Enter the no. of matrix: " << endl;
+    cin >> t;
+
+    int a[t][t];
+    for (int i = 0; i < t; i++)
+    {
+        for (int j = 0; j < t; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    int currentstate = 0;
+    for (int i = 0; i < n.size(); i++)
+    {
+        char c = n[i];
+        bool match_found = false;
+
+        if (is_char(c))
+        {
+            for (int j = 0; j < t; j++)
+            {
+                if (c == char(a[i][j]))
+                {
+                    match_found = true;
+                    break;
+                }
+            }
+        }
+        else if (is_num(c))
+        {
+            int numeric_value = c - '0';
+            for (int j = 0; j < t; j++)
+            {
+                if ((numeric_value == a[i][j] || a[i][j] == -1))
+                {
+                    match_found = true;
+                    break;
+                }
+            }
+        }
+
+        if (!match_found)
+        {
+            currentstate = 1;
+            break;
+        }
+    }
+
+    if (currentstate == 1)
+    {
+        cout << "Accepted!!!" << endl;
+    }
+    else
+    {
+        cout << "Not Accepted!!!" << endl;
+    }
     return 0;
 }
